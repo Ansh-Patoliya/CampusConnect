c15eee0597dee2fc4d2dbd331568d35ca49ebe21
ervrajai <vrajpatelbhikhabhai@gmail.com>
2025-07-13 23:07:17 +0530

    Forgot Password

--- src/com/eventApp/Service/UserService.java
+++ src/com/eventApp/Service/UserService.java
@@ -10,4 +10,8 @@ public class UserService {
     }
 
     public boolean checklogin(String emailInput, String passwordInput) { return userDAO.checkLoginDetails(emailInput, passwordInput); }
+
+    public boolean resetPassword(String emailInput, String newPassword, String confirmPassword) {
+        return UserDAO.resetPass(emailInput,newPassword,confirmPassword);
+    }
 }
