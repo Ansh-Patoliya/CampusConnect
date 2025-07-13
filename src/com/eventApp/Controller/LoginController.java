2290b47437a7212a71f291d03b40fca2b1c64f70
mp12github <mudrapatel93@gmail.com>
2025-07-13 17:56:20 +0530

    updated

---
+++ src/com/eventApp/Controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.eventApp.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    public TextField username;
+
+    public void openSignupPage(ActionEvent actionEvent) {
+        openSignupPage(actionEvent);
+    }
+}
