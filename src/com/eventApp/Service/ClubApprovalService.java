40e48a3bc264caaf24c0a8a78e302271ae507422
Jiya Govani <jiyagovani12900@gmail.com>
2025-07-19 22:14:27 +0530

    ClubApprovalService updated

---
+++ src/com/eventApp/Service/ClubApprovalService.java
@@ -0,0 +1,63 @@
+package com.eventApp.Service;
+import com.eventApp.DAO.AdminDAO;
+import com.eventApp.DAO.ClubDAO;
+import com.eventApp.Model.Club;
+import com.eventApp.DataStructures.MyClubQueue;
+import com.eventApp.Utils.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ClubApprovalService {
+
+    private AdminDAO adminDAO;
+    private MyClubQueue queue= adminDAO.getClubList("pending");
+    private ClubDAO clubDAO;
+
+    public Club viewNextPendingClub() {
+        // Returns next pending club
+        return queue.peek();
+    }
+
+    public boolean approveNextClub(String clubId) {
+        // Approve and remove club from queue
+        try(Connection connection = DatabaseConnection.getConnection()){
+            String query = "UPDATE clubs SET status = 'approved' WHERE id = ? AND status = 'pending'";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, clubId);
+            int rowsUpdated = preparedStatement.executeUpdate();
+            if(rowsUpdated>0){
+                queue.dequeue();
+                return true;
+            } else{
+                return false;
+            }
+        } catch (SQLException | ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean rejectNextClub(String clubId) {
+        // Reject and remove club from queue
+        try(Connection connection = DatabaseConnection.getConnection()){
+            String query = "UPDATE clubs SET status = 'rejected' WHERE id = ? AND status = 'pending'";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, clubId);
+            int rowsUpdated = preparedStatement.executeUpdate();
+            if(rowsUpdated>0){
+                queue.dequeue();
+                return true;
+            } else{
+                return false;
+            }
+        } catch (SQLException | ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public MyClubQueue getAllPendingClubs() {
+        // Return pending clubs from DB
+        return queue;
+    }
+}
