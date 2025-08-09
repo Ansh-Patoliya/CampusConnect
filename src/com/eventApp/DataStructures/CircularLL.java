b3af83c02f3828690abb7beb05e00b74a1e3cedc
Ansh-Patoliya <anshpatoliya1408@gmail.com>
2025-08-09 11:55:35 +0530

    Implement event management enhancements with CircularLL for event retrieval and cancellation

---
+++ src/com/eventApp/DataStructures/CircularLL.java
@@ -0,0 +1,72 @@
+package com.eventApp.DataStructures;
+
+import com.eventApp.Model.Event;
+
+public class CircularLL {
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    class Node{
+        Event data;
+        Node next;
+        Node(Event data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    private Node head=null;
+    private Node current=null;
+
+    // insert sorted by date
+    public void add(Event data){
+        Node n = new Node(data);
+        if (head == null) {
+            head = n;
+            n.next = head;
+            return;
+        }
+        if (comesBefore(data, head.data)) {
+            Node last = head;
+            while (last.next != head) {
+                last = last.next;
+            }
+            last.next = n;
+            n.next = head;
+            head = n;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != head && comesBefore(temp.next.data, data)) {
+            temp = temp.next;
+        }
+        n.next = temp.next;
+        temp.next = n;
+    }
+
+    private boolean comesBefore(Event a, Event b) {
+        if (a.getEventDate().isBefore(b.getEventDate())) {
+            return true;
+        }
+        if (a.getEventDate().isEqual(b.getEventDate())) {
+            return a.getApprovalStatus().compareTo(b.getApprovalStatus()) < 0;
+        }
+        return false;
+    }
+
+
+    public Event viewCurrentEvent() {
+        if (current == null) {
+            return null; // No events in the list
+        }
+        return current.data;
+    }
+
+    public Event viewNextEvent() {
+        if (current == null) {
+            return null; // No events in the list
+        }
+        current = current.next;
+        return current.data;
+    }
+}
--- src/com/eventApp/Service/ClubService.java
+++ src/com/eventApp/Service/ClubService.java
@@ -4,6 +4,8 @@ import com.eventApp.DAO.ClubDAO;
 import com.eventApp.DAO.ClubMemberDAO;
 import com.eventApp.DAO.EventDAO;
 import com.eventApp.DAO.EventRegistrationDAO;
+import com.eventApp.DataStructures.CircularLL;
+import com.eventApp.ExceptionHandler.DatabaseExceptionHandler;
 import com.eventApp.Model.*;
 
 import java.io.BufferedWriter;
@@ -18,7 +20,11 @@ public class ClubService {
     public final ClubDAO clubDAO = new ClubDAO();
     public final ClubMemberDAO clubMemberDAO = new ClubMemberDAO();
     public final EventRegistrationDAO eventRegistrationDAO = new EventRegistrationDAO();
-    public final EventDAO eventDAO= new EventDAO();
+    public final EventDAO eventDAO = new EventDAO();
+
+    public ClubService() {
+        // Default constructor
+    }
 
     public boolean addEvent(Event event) {
         return clubDAO.createEvent(event);
@@ -26,11 +32,7 @@ public class ClubService {
 
     public Club getClubByUser(User user) {
         int clubId = ClubMemberDAO.getClubMember(user).getClubId();
-        Club club = clubDAO.getClubById(clubId);
-        if (club == null) {
-            return null; // No club found for the user
-        }
-        return club;
+        return clubDAO.getClubById(clubId);
     }
 
     public List<ClubMember> getClubMember(User user) throws SQLException, ClassNotFoundException {
@@ -44,7 +46,6 @@ public class ClubService {
         participants.sort(Comparator.comparing(Student::getName));
         return participants;
     }
-
 
     public void exportClubMembersToCSV(List<ClubMember> clubMemberList, String filePath) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
@@ -68,4 +69,28 @@ public class ClubService {
         Collections.sort(eventNameList);
         return eventNameList;
     }
+
+    CircularLL myEventLL;
+
+    public ClubService(User user) throws SQLException, DatabaseExceptionHandler, ClassNotFoundException {
+        loadMyEventList(user);
+    }
+
+    public void loadMyEventList(User user) throws SQLException, ClassNotFoundException, DatabaseExceptionHandler {
+        int clubId = ClubMemberDAO.getClubMember(user).getClubId();
+        this.myEventLL = eventDAO.getEventListByClubId(clubId);
+    }
+
+    public Event viewCurrentEvent() {
+        return myEventLL.viewCurrentEvent();
+    }
+
+    public Event viewNextEvent() {
+        return myEventLL.viewNextEvent();
+    }
+
+    public void cancelEvent() throws SQLException, DatabaseExceptionHandler, ClassNotFoundException {
+        eventDAO.cancelEvent(viewCurrentEvent().getEventId());
+    }
+
 }
